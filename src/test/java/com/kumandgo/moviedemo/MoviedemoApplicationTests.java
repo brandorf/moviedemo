59f0bd461f7bdeca2021838c1462486c15997e69
Brandon Kiesling <brandon.kiesling@kumandgo.com>
2024-02-07 11:10:05 -0600

    Initial commit

---
+++ src/test/java/com/kumandgo/moviedemo/MoviedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumandgo.moviedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
